91ead3a33a368c54efc09be8bfef75fed2c7eb9c
Nilesh <nilesh15394@gmail.com>
2020-04-05 13:21:53 +0530

    New file

--- src/main/java/com/demo/git/GitDemoApplication.java
+++ src/main/java/com/demo/git/GitDemoApplication.java
@@ -9,6 +9,11 @@ public class GitDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
 		System.out.println("Application started >>>>>>>>>>>>>");
+		printMyName();
+	}
+
+	private static void printMyName() {
+		System.out.println("Nilesh Narkhede");
 	}
 
 }
